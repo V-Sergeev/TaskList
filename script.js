4d07280a437cfc418c3485495f684d98b9bd4394
V-Sergeev <sergeev.valer@gmail.com>
2016-10-07 22:46:43 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
-; angular.module('appTasks',[]);
-          
-          angular.module('appTasks').
-          directive('myDir', function(){
-            return {
-              scope: true,
-              restrict: 'E',
-              link: function(scope){
-                scope.tasks=[];
-                scope.hide=false;
-                scope.addTask=function(){
-                  scope.tasks.push(scope.task);
-                  scope.task='';
-                };
-                scope.showTasks=function(){
-                  alert(scope.tasks);
-                };
-              }
-            };
-          });
-          
+;
+angular.module('appTasks', []);
+
+angular.module('appTasks').
+directive('myDir', function() {
+  return {
+    scope: true,
+    restrict: 'E',
+    link: function(scope) {
+      scope.tasks = [];
+      scope.st = "yes-no";
+      
+      scope.addTask = function() {
+        console.log(scope.task);
+        if (!!scope.task.date && !!scope.task.name) {
+          scope.task.status = "yes";
+          scope.tasks.push(scope.task);
+          scope.task = "";
+         // console.log(scope.tasks);
+        }
+      };
+      scope.showTasks = function() {
+      console.log(scope.tasks);
+      };
+    }
+  };
+});
